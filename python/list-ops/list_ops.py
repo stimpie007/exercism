b13985421b74908f2d23913e77620a129405b9ab
stevenhiltrop <stevenhiltrop@gmail.com>
2021-01-10 16:58:05 +0700

    Added list_ops.py solution

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,30 +1,40 @@
-def append(list1, list2):
-    pass
+def _cons(x, xs):
+    return [x, *xs]
+
+
+def append(xs, ys):
+    return foldr(_cons, xs, ys)
 
 
 def concat(lists):
-    pass
+    return foldl(append, lists, [])
 
 
-def filter(function, list):
-    pass
+def filter(function, xs):
+    return foldr(lambda x, acc: _cons(x, acc) if function(x) else acc, xs, [])
 
 
-def length(list):
-    pass
+def length(xs):
+    return foldl(lambda acc, x: acc + 1, xs, 0)
 
 
-def map(function, list):
-    pass
+def map(function, xs):
+    return foldr(lambda x, acc: _cons(function(x), acc), xs, [])
 
 
-def foldl(function, list, initial):
-    pass
+def foldl(function, xs, acc):
+    if len(xs) == 0:
+        return acc
+    x, *rxs = xs
+    return foldl(function, rxs, function(acc, x))
 
 
-def foldr(function, list, initial):
-    pass
+def foldr(function, xs, acc):
+    if len(xs) == 0:
+        return acc
+    x, *rxs = xs
+    return function(x, foldr(function, rxs, acc))
 
 
-def reverse(list):
-    pass
+def reverse(xs):
+    return foldl(lambda acc, x: _cons(x, acc), xs, [])
