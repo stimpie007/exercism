e8a5f6fba908114be23b80f8def752742fbaeeed
stevenhiltrop <stevenhiltrop@gmail.com>
2021-01-10 16:54:32 +0700

    Added binary_search.py solution

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,12 @@
 def find(search_list, value):
-    pass
+    left_most = 0
+    right_most = len(search_list) - 1
+    while left_most <= right_most:
+        m = (left_most + right_most) // 2
+        if search_list[m] < value:
+            left_most = m + 1
+        if search_list[m] > value:
+            right_most = m - 1
+        if search_list[m] == value:
+            return m
+    raise ValueError(f"{value} novalue found in {search_list}")
