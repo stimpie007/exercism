161aaeceeab3b0327a81a89d17bb2ee7dfdcad27
stevenhiltrop <stevenhiltrop@gmail.com>
2021-03-15 17:59:57 +0700

    Added simple_linked_list.py solution

--- python/simple-linked-list/simple_linked_list.py
+++ python/simple-linked-list/simple_linked_list.py
@@ -9,38 +9,59 @@ class Node:
         pass
 
 
-class LinkedList:
+class Node(object):
+    def __init__(self, value):
+        self.value_ = value
+        self.next_ = None
+
+    def value(self):
+        return self.value_
+
+    def next(self):
+        return self.next_
+
+class LinkedList(object):
     def __init__(self, values=[]):
-        self.values = values
-        self._value = None
+        self.values = []
+        previous = None
+        for v in values:
+            node = Node(v)
+            self.values = [node] + self.values
+            if previous is None:
+                previous = node
+            else:
+                node.next_ = previous
+                previous = node
 
     def __len__(self):
         return len(self.values)
 
     def head(self):
         if not self.values:
-            raise EmptyListException()
-        self._value = self.values[-1]
-        return self
-
-    def value(self):
-        return self._value
-
-    def next(self):
-        _values = iter(self.values)
-        self._value = next(_values)
-        return self
+            raise EmptyListException("Empty list")
+        return self.values[0]
 
     def push(self, value):
-        self.values.append(value)
+        node = Node(value)
+        if self.values:
+            node.next_ = self.values[0]
+        self.values = [node] + self.values
 
     def pop(self):
-        return self.values.pop()
+        if not self.values:
+            raise EmptyListException("Empty list")
+        node = self.values[0]
+        self.values = self.values[1:]
+        return node.value()
 
     def reversed(self):
-        pass
+        for i in reversed(self.values):
+            yield i.value()
+
+    def __iter__(self):
+        for i in self.values:
+            yield i.value()
 
 
 class EmptyListException(Exception):
-    # raise ValueError("Pretty empty list eh.")
     pass
