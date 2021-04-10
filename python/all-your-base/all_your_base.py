e4704e6eba8d3ff49aa64cf119f8465d658c113d
stevenhiltrop <stevenhiltrop@gmail.com>
2021-04-10 16:55:14 +0700

    Added all_your_base.py solution

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,2 +1,9 @@
 def rebase(input_base, digits, output_base):
-    pass
+    if input_base < 2 or sum(filter(lambda x: x < 0 or x >= input_base, digits)) != 0 or output_base < 2:
+        raise ValueError("wrong arguments")
+    value = sum(map(lambda x: x[1] * input_base ** x[0], enumerate(reversed(digits))))
+    output_digits = []
+    while value > 0:
+        output_digits.insert(0, value % output_base)
+        value //= output_base
+    return output_digits
