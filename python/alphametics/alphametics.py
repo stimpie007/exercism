ccd4bb9d2b949edb095055243e387496cce77219
stevenhiltrop <stevenhiltrop@gmail.com>
2021-04-10 17:03:37 +0700

    Added alphametics.py solution

--- python/alphametics/alphametics.py
+++ python/alphametics/alphametics.py
@@ -1,2 +1,15 @@
+from itertools import permutations
+
+
 def solve(puzzle):
-    pass
+    words = [w for w in puzzle.split() if w.isalpha()]
+    nonzero = set([w[0] for w in words])
+    letters = list(set(''.join(words)) - nonzero) + list(nonzero)
+    perms = permutations('0123456789', len(letters))
+    for perm in perms:
+        conv_dict = dict(zip(letters, perm))
+        if '0' in perm[-len(nonzero):]:
+            continue
+        if eval(''.join([conv_dict[c] if c.isalpha() else c for c in puzzle])):
+            return {k: int(v) for k, v in conv_dict.items()}
+    return {}
