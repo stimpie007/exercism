63fceee88a0963dd2020b6c3d03657ee5b326cff
stevenhiltrop <stevenhiltrop@gmail.com>
2021-04-10 17:02:08 +0700

    Added word_search.py solution

--- python/word-search/word_search.py
+++ python/word-search/word_search.py
@@ -1,7 +1,7 @@
 class Point:
     def __init__(self, x, y):
-        self.x = None
-        self.y = None
+        self.x = x
+        self.y = y
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
@@ -9,7 +9,28 @@ class Point:
 
 class WordSearch:
     def __init__(self, puzzle):
-        pass
+        self.puzzle = puzzle
 
     def search(self, word):
-        pass
+        width, height = len(self.puzzle[0]) - 1, len(self.puzzle) - 1
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1),  # → ← ↓ ↑
+                      (-1, -1), (1, -1), (-1, 1), (1, 1))  # ↖ ↗ ↙ ↘
+        # Storing co-ordinates of all the points in the puzzle that match first alphabet in the word to be searched
+        points = [(x, y) for x in range(width + 1) for y in range(height + 1) if self.puzzle[y][x] == word[0]]
+        # start iterating over all the matched first character of the target word
+        for x, y in points:
+            start_coordinates = Point(x, y)
+            # search through all the possible directions for a given set of coordinates
+            for direction in directions:
+                line = []
+                x2, y2 = x, y
+                while 0 <= x2 <= width and 0 <= y2 <= height:
+                    line.append(self.puzzle[y2][x2])
+                    # if found match return the matched start and the end cordinates
+                    if word == ''.join(line):
+                        end_cordinates = Point(x2, y2)
+                        return start_coordinates, end_cordinates
+                    else:
+                        x2 += direction[0]
+                        y2 += direction[1]
+        return None
