27746b63ebdc4af1c3e88ecfed50fc7f77e1b110
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-17 19:43:15 +0700

    Added collatz_conjecture logic

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,21 @@
+def input_validation(number):
+    if number <= 0:
+        raise ValueError(f"Input {number} is not a positive integer")
+
+
 def steps(number):
-    pass
+    input_validation(number)
+
+    amountofsteps = 0
+
+    while not (number == 1):
+        # odd
+        if number % 2 != 0:
+            number = (number * 3) + 1
+        # even
+        if number % 2 == 0:
+            number = number / 2
+
+        amountofsteps += 1
+
+    return amountofsteps
