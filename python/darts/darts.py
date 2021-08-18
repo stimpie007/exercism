6e55748a6fbeb548bee8f199c044092f907c95ea
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-18 11:01:53 +0700

    Changed math functions to build in operator for slight performance increase.

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,5 +1,3 @@
-from math import pi, sqrt, pow
-
 #  Points
 OUTER_CIRKLE = 1
 MIDDLE_CIRKLE = 5
@@ -7,14 +5,15 @@ INNER_CIRKLE = 10
 
 
 def score(x, y):
-    radius = sqrt(pow(x, 2) + pow(y, 2))
+    radius = (x ** 2 + y ** 2) ** 0.5
+
     if radius > 10:
         return 0
-    elif 10 > radius > 5:
+    elif 10 >= radius > 5:
         return OUTER_CIRKLE
-    elif 5 > radius > 1:
+    elif 5 >= radius > 1:
         return MIDDLE_CIRKLE
-    elif 1 > radius:
+    elif 1 >= radius:
         return INNER_CIRKLE
     else:
         raise Exception("You threw the dart at one eyed Pete.")
