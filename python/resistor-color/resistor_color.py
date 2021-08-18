a9f149237da2153585433b245622e7b71ff03636
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-18 19:52:37 +0700

    Added functionality

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,20 @@
-def color_code(color):
-    pass
+COLORS = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
 
 
-def colors():
-    pass
+def color_code(color: str) -> int:
+    return COLORS[color]
+
+
+def colors() -> list:
+    return list(COLORS)
