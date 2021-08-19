8adad9a4deac643ac187837de53d894723c64e29
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-19 09:34:28 +0700

    Added functionality, typing and documenation.

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,28 @@
-def value(colors):
-    pass
+COLORS = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
+
+def value(colors: list(str)) -> int:
+    """
+    Get the resistor code for given color codes
+
+    Args:
+        colors: list(str)
+            Colors look for resistor code
+
+    Returns:
+        resistor_code: int
+            Color code for the given resistors
+    """
+    resistors = [COLORS[color] for color in colors][:2]
+    return int("".join(map(str, resistors)))
