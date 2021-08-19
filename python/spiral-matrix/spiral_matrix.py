b5592134935c7ffd704ec6336a2cc0b3ac152695
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-19 11:07:05 +0700

    Added functionality

--- python/spiral-matrix/spiral_matrix.py
+++ python/spiral-matrix/spiral_matrix.py
@@ -1,2 +1,24 @@
+from itertools import cycle
+
+
 def spiral_matrix(size):
-    pass
+    matrix = [[None] * size for _ in range(size)]
+    r, c = 0, 0
+
+    # this cycle determines the movement of the "current cell"
+    # (0,1) represents moving along a row to the right
+    # (1,0) represents moving down a column
+    deltas = cycle(((0,1), (1,0), (0,-1), (-1,0)))
+    dr, dc = next(deltas)
+
+    for i in range(size**2):
+        matrix[r][c] = i+1
+        if (
+            not 0 <= r+dr < size or
+            not 0 <= c+dc < size or
+            matrix[r+dr][c+dc] is not None
+        ):
+            dr, dc = next(deltas)
+        r += dr
+        c += dc
+    return matrix
