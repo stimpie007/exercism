0eb679cb17f084870ac1f9432b0eea4040642a9f
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-21 20:29:04 +0700

    Added enum to keep state and the lock library to make asynchronous transactions possible.

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,43 +1,80 @@
-def check_balance(balance):
-    if balance is None:
-        raise ValueError("Na-a")
+"""
+Exercism solution for "bank-account"
+"""
+from enum import Enum
+from threading import Lock
 
 
-def check_amount(amount):
-    if amount < 0:
-        raise ValueError("Trying to pull a short one?")
+class State(Enum):
+    """
+    The various states of a bank account.
+    """
+
+    UNOPENED = "unopened"
+    OPEN = "open"
+    CLOSED = "closed"
 
 
-class BankAccount:
-    def __init__(self):
-        self.balance = None
-        self.hasAccount = False
+class BankAccount(object):
+    """
+    Very simple Bank Account with transaction locking.
+    """
 
-    def open(self):
-        if self.hasAccount:
-            raise ValueError("You have an open account")
+    def __init__(self) -> None:
+        self._balance = 0
+        self._state = State.UNOPENED
+        self._lock = Lock()
 
-        self.balance = 0
-        self.hasAccount = True
+    def _error_if_not_open(self) -> None:
+        if self._state is State.UNOPENED:
+            raise ValueError("Account never opened!")
+        if self._state is State.CLOSED:
+            raise ValueError("Account not open!")
 
-    def close(self):
-        check_balance(self.balance)
-        self.hasAccount = False
-        self.balance = None
+    def open(self) -> None:
+        """
+        Open the account and initialize the balance.
+        """
+        with self._lock:
+            if self._state is State.OPEN:
+                raise ValueError("Account already open!")
+            self._state = State.OPEN
+            self._balance = 0
 
-    def get_balance(self):
-        check_balance(self.balance)
-        return self.balance
+    def close(self) -> None:
+        """
+        Close the account.
+        """
+        with self._lock:
+            self._error_if_not_open()
+            self._state = State.CLOSED
 
-    def deposit(self, amount):
-        check_balance(self.balance)
-        check_amount(amount)
-        self.balance += amount
+    def get_balance(self) -> int:
+        """
+        Get the current balance of the account.
+        """
+        with self._lock:
+            self._error_if_not_open()
+            return self._balance
 
-    def withdraw(self, amount):
-        check_balance(self.balance)
-        check_amount(amount)
-        if (self.balance - amount) >= 0:
-            self.balance -= amount
-        else:
-            raise ValueError("Not enough in the bank")
+    def deposit(self, amount: int) -> None:
+        """
+        Deposit amount into account. Raises ValueError on negative amounts.
+        """
+        with self._lock:
+            self._error_if_not_open()
+            if amount < 0:
+                raise ValueError("Cannot make a negative deposit!")
+            self._balance += amount
+
+    def withdraw(self, amount: int) -> None:
+        """
+        Withdraw amount from account. Raises ValueError on negative or overdraft withdrawls.
+        """
+        with self._lock:
+            self._error_if_not_open()
+            if amount < 0:
+                raise ValueError("Cannot make a negative withdrawl!")
+            elif amount > self._balance:
+                raise ValueError("Cannot overdraw account!")
+            self._balance -= amount
