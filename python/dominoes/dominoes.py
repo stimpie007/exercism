b236b6ed8d81ae3e86f82d99468b7261604453af
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-21 18:39:22 +0700

    Added dominoes solution

--- python/dominoes/dominoes.py
+++ python/dominoes/dominoes.py
@@ -1,2 +1,27 @@
 def can_chain(dominoes):
-    pass
+    return next(
+        chain(dominoes, []),
+        None if dominoes else []
+    )
+
+
+def chain(tiles, ordered):
+    if (not tiles and ordered
+            and ordered[0][0] == ordered[-1][1]):
+        yield ordered
+
+    for t in tiles:
+        idx = tiles.index(t)
+        rest_tiles = tiles[:idx] + tiles[idx + 1:]
+        rev = t[::-1]
+
+        if not ordered:
+            yield from chain(rest_tiles, [t])
+        elif t[0] == ordered[-1][1]:
+            yield from chain(rest_tiles, ordered + [t])
+        elif t[0] == ordered[0][0]:
+            yield from chain(rest_tiles, [rev] + ordered)
+        elif t[1] == ordered[0][0]:
+            yield from chain(rest_tiles, [t] + ordered)
+        elif t[1] == ordered[-1][1]:
+            yield from chain(rest_tiles, ordered + [rev])
