ff56f69c3580fe801d3be7edc8c645042f91838a
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-21 20:20:23 +0700

    Added functionality. Failing the asynchronous depositing test.

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,18 +1,43 @@
+def check_balance(balance):
+    if balance is None:
+        raise ValueError("Na-a")
+
+
+def check_amount(amount):
+    if amount < 0:
+        raise ValueError("Trying to pull a short one?")
+
+
 class BankAccount:
     def __init__(self):
-        pass
-
-    def get_balance(self):
-        pass
+        self.balance = None
+        self.hasAccount = False
 
     def open(self):
-        pass
+        if self.hasAccount:
+            raise ValueError("You have an open account")
 
-    def deposit(self, amount):
-        pass
-
-    def withdraw(self, amount):
-        pass
+        self.balance = 0
+        self.hasAccount = True
 
     def close(self):
-        pass
+        check_balance(self.balance)
+        self.hasAccount = False
+        self.balance = None
+
+    def get_balance(self):
+        check_balance(self.balance)
+        return self.balance
+
+    def deposit(self, amount):
+        check_balance(self.balance)
+        check_amount(amount)
+        self.balance += amount
+
+    def withdraw(self, amount):
+        check_balance(self.balance)
+        check_amount(amount)
+        if (self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            raise ValueError("Not enough in the bank")
