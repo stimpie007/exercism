9b6f0e47e0c887e424600892d48648639e7b14e5
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-22 16:14:24 +0700

    Added pov solution

--- python/pov/pov.py
+++ python/pov/pov.py
@@ -1,25 +1,46 @@
 from json import dumps
 
-
 class Tree:
-    def __init__(self, label, children=[]):
+    def __init__(self, label, children=None):
         self.label = label
-        self.children = children
+        self.children = children or []
 
-    def __dict__(self):
-        return {self.label: [c.__dict__() for c in sorted(self.children)]}
+    def __dict(self):
+        return {self.label: [c.__dict() for c in sorted(self.children)]}
 
     def __str__(self, indent=None):
-        return dumps(self.__dict__(), indent=indent)
+        return dumps(self.__dict(), indent=indent)
 
     def __lt__(self, other):
         return self.label < other.label
 
     def __eq__(self, other):
-        return self.__dict__() == other.__dict__()
+        return self.__dict() == other.__dict()
 
     def from_pov(self, from_node):
-        pass
+        path = []
+        if not self._search(from_node, self, path):
+            raise ValueError(f"node {from_node} does not exist")
+        for i, node in enumerate(path):
+            if i < len(path) - 1:
+                node.children.remove(path[i + 1])
+            if i > 0:
+                node.children.append(path[i - 1])
+        return path[-1]
 
     def path_to(self, from_node, to_node):
-        pass
+        root, path = self.from_pov(from_node), []
+        if not self._search(to_node, root, path):
+            raise ValueError(f"node {to_node} does not exist")
+        return [node.label for node in path]
+
+    def _search(self, label, node, path):
+        path.append(node)
+        if node.label == label:
+            return True
+
+        for c in node.children:
+            if self._search(label, c, path):
+                return True
+            path.pop()
+        return False
