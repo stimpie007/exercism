4756e9c53e62fd46194229a0839e22209b3d5a0f
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-22 15:46:47 +0700

    Added knapsack solution

--- python/knapsack/knapsack.py
+++ python/knapsack/knapsack.py
@@ -1,2 +1,6 @@
 def maximum_value(maximum_weight, items):
-    pass
+    combinations = [(0, 0)]
+    for item in items:
+        combinations += [(w + item['weight'], v + item['value']) for w, v in combinations]
+
+    return max(v for w, v in combinations if w <= maximum_weight)
