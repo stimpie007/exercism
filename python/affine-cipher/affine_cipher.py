16a22da8d168493dd336827a07f1665b320a5f39
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-22 15:39:52 +0700

    Added affine_cipher solution

--- python/affine-cipher/affine_cipher.py
+++ python/affine-cipher/affine_cipher.py
@@ -1,6 +1,24 @@
+LETTER_BASE = ord('a')
+ALPHABET_LEN = 26
+
+
 def encode(plain_text, a, b):
-    pass
+    _ = inverse(a)  # Validate the coprime
+    encoded = "".join([chr(LETTER_BASE + ((a * (ord(x) - LETTER_BASE) + b) % ALPHABET_LEN))
+                       if x.isalpha() else x for x in plain_text.lower() if x.isalnum()])
+    return " ".join([encoded[i:i + 5] for i in range(0, len(encoded), 5)])
 
 
 def decode(ciphered_text, a, b):
-    pass
+    a_inverse = inverse(a)
+    return "".join(
+        [chr(LETTER_BASE + (int(a_inverse * (ord(y) - LETTER_BASE - b))) % ALPHABET_LEN) if y.isalpha() else y for y in
+         ciphered_text.lower() if y.isalnum()])
+
+
+def inverse(a):
+    result = [x for x in range(1, ALPHABET_LEN) if a * x % ALPHABET_LEN == 1]
+    if len(result) >= 1:
+        return result[0]
+    else:
+        raise ValueError("Error: a and m must be coprime.")
