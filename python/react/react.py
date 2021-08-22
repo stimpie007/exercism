bd859bf5d5609763e956eb7562982e9d9978ee9e
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-22 16:12:17 +0700

    Added react solution using observer

--- python/react/react.py
+++ python/react/react.py
@@ -1,14 +1,58 @@
-class InputCell:
+class Cell:
     def __init__(self, initial_value):
-        self.value = None
+        self._value = initial_value
+        self._listeners = set()
+
+    def add_listener(self, cell):
+        self._listeners.add(cell)
+
+    def notify_listeners(self, changed):
+        for listener in self._listeners:
+            listener.recompute(changed)
+
+    @property
+    def value(self):
+        return self._value
 
 
-class ComputeCell:
+class InputCell(Cell):
+    @Cell.value.setter
+    def value(self, val):
+        if val != self._value:
+            self._value = val
+            # Map modified ComputeCells to their initial values.
+            changed = {}
+            self.notify_listeners(changed)
+            for cell, original_value in changed.items():
+                if cell.value != original_value:
+                    cell.call_callbacks()
+
+
+class ComputeCell(Cell):
     def __init__(self, inputs, compute_function):
-        self.value = None
+        self._inputs = inputs
+        self._compute_function = compute_function
+        self._callbacks = set()
+        for i in inputs:
+            i.add_listener(self)
+        super().__init__(self.compute())
 
     def add_callback(self, callback):
-        pass
+        self._callbacks.add(callback)
 
     def remove_callback(self, callback):
-        pass
+        self._callbacks.discard(callback)
+
+    def compute(self):
+        return self._compute_function([i.value for i in self._inputs])
+
+    def recompute(self, changed):
+        val = self.compute()
+        if val != self.value:
+            changed.setdefault(self, self.value)
+            self._value = val
+            self.notify_listeners(changed)
+
+    def call_callbacks(self):
+        for callback in self._callbacks:
+            callback(self.value)
