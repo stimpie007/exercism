1aa745ce8bb1bf4a562daea3917bb74e54ef15f2
stevenhiltrop <stevenhiltrop@gmail.com>
2021-11-16 12:20:34 +0700

    Added tisbury-treasure-hunt get_coordinate()

--- python/tisbury-treasure-hunt/tuples.py
+++ python/tisbury-treasure-hunt/tuples.py
@@ -4,8 +4,7 @@ def get_coordinate(record):
     :param record: tuple - a (treasure, coordinate) pair.
     :return: str - the extracted map coordinate.
     """
-
-    pass
+    return record[1]
 
 
 def convert_coordinate(coordinate):
