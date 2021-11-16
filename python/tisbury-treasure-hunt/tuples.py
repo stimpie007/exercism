bdf86e8c1caf56c2c8212615f2783522adf5b90d
stevenhiltrop <stevenhiltrop@gmail.com>
2021-11-16 12:26:50 +0700

    Added tisbury-treasure-hunt convert_coordinate()

--- python/tisbury-treasure-hunt/tuples.py
+++ python/tisbury-treasure-hunt/tuples.py
@@ -13,8 +13,7 @@ def convert_coordinate(coordinate):
     :param coordinate: str - a string map coordinate
     :return:  tuple - the string coordinate seperated into its individual components.
     """
-
-    pass
+    return tuple(coordinate)
 
 
 def compare_records(azara_record, rui_record):
