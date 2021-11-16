da548fa7cf586baadc8286eb05176a081adc3770
stevenhiltrop <stevenhiltrop@gmail.com>
2021-11-16 12:04:15 +0700

    Changed making-the-grade to be more pythonic using build-in iterators.

--- python/making-the-grade/loops.py
+++ python/making-the-grade/loops.py
@@ -3,7 +3,7 @@ def round_scores(student_scores):
     :param student_scores: list of student exam scores as float or int.
     :return: list of student scores *rounded* to nearest integer value.
     """
-    return [round(score) for score in student_scores]
+    return list(map(round, student_scores))
 
 
 def count_failed_students(student_scores):
@@ -11,7 +11,7 @@ def count_failed_students(student_scores):
     :param student_scores: list of integer student scores.
     :return: integer count of student scores at or below 40.
     """
-    return sum([score <= 40 for score in student_scores])
+    return sum(map(lambda n: n <= 40, student_scores))
 
 
 def above_threshold(student_scores, threshold):
@@ -20,7 +20,7 @@ def above_threshold(student_scores, threshold):
     :param threshold :  integer
     :return: list of integer scores that are at or above the "best" threshold.
     """
-    return [score for score in student_scores if score >= threshold]
+    return list(filter(lambda n: n >= threshold, student_scores))
 
 
 def letter_grades(highest):
@@ -28,8 +28,7 @@ def letter_grades(highest):
     :param highest: integer of highest exam score.
     :return: list of integer score thresholds for each F-A letter grades.
     """
-    threshold = 41
-    return list(range(threshold, highest, round((highest - threshold) / 4)))
+    return list(range(41, highest, round((highest - 41) / 4)))
 
 
 def student_ranking(student_scores, student_names):
@@ -39,8 +38,8 @@ def student_ranking(student_scores, student_names):
      :return: list of strings in format ["<rank>. <student name>: <score>"].
      """
     # There should be a buildin argument in zip to use its current index..
-    return [f"{index + 1}. {student}: {score}" for index, (student, score) in
-            enumerate(zip(student_names, student_scores))]
+    return list(f"{index + 1}. {student_names[index]}: {student_scores[index]}"
+                for index in range(len(student_scores)))
 
 
 def perfect_score(student_info):
