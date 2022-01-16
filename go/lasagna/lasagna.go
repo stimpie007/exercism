92c93c3e54b35dda995f5b0b7589e979f7d748db
stevenhiltrop <stevenhiltrop@gmail.com>
2022-01-16 18:00:14 +0700

    Added OvenTime constant

--- go/lasagna/lasagna.go
+++ go/lasagna/lasagna.go
@@ -1,6 +1,7 @@
 package lasagna
 
 // TODO: define the 'OvenTime' constant
+const OvenTime = 40
 
 // RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
 func RemainingOvenTime(actualMinutesInOven int) int {
