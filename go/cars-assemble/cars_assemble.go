fbfe1c3777944faf562a64e1332b382dbd310c90
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-14 19:03:29 +0700

    Added CalculateWorkingCarsPerHour functionality

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -3,7 +3,7 @@ package cars
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	return float64(productionRate) / 100 * successRate
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
