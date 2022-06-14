92e690b4d6f841947db8973ce71d2cc4f82d4188
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-14 16:35:34 +0700

    Added CanSpy code

--- go/annalyns-infiltration/annalyns_infiltration.go
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -7,7 +7,8 @@ func CanFastAttack(knightIsAwake bool) bool {
 
 // CanSpy can be executed if at least one of the characters is awake
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
-	panic("Please implement the CanSpy() function")
+	return knightIsAwake || archerIsAwake || prisonerIsAwake
+
 }
 
 // CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping
