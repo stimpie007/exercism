3b06ade7c15c03757c7af1577dd72b7b98f622eb
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-18 11:19:37 +0700

    Added ChooseVechicle code

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -1,13 +1,21 @@
 package purchase
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	return !strings.Contains(kind, "not")
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	options := []string{option1, option2}
+	sort.Strings(options)
+	return fmt.Sprintf("%s is clearly the better choice.", options[0])
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
