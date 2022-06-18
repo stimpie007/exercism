508d585a7538b5705f00d59f69605122d2c0b9f4
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-18 11:53:00 +0700

    Refactored ChooseVechicle code

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -1,10 +1,5 @@
 package purchase
 
-import (
-	"fmt"
-	"sort"
-)
-
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
 	return kind == "car" || kind == "truck"
@@ -12,9 +7,14 @@ func NeedsLicense(kind string) bool {
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	options := []string{option1, option2}
-	sort.Strings(options)
-	return fmt.Sprintf("%s is clearly the better choice.", options[0])
+	message := func(option string) string {
+		return option + " is clearly the better choice."
+	}
+
+	if option1 < option2 {
+		return message(option1)
+	}
+	return message(option2)
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
