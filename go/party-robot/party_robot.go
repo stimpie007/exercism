7fb2c0eb1ecd85d816ddec9e125154b0a52ce0b0
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-20 21:11:54 +0700

    Added AssignTable code

--- go/party-robot/party_robot.go
+++ go/party-robot/party_robot.go
@@ -14,5 +14,8 @@ func HappyBirthday(name string, age int) string {
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	panic("Please implement the AssignTable function")
+	return fmt.Sprintf("Welcome to my party, %s!\n"+
+		"You have been assigned to table %.3d. Your table is %s, exactly %.1f meters from here.\n"+
+		"You will be sitting next to %s.",
+		name, table, direction, distance, neighbor)
 }
