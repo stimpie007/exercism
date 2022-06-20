69f9d2f14063bc328f408b6992fb85698fb14478
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-20 21:20:02 +0700

    Added FavoriteCards code

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -2,7 +2,7 @@ package cards
 
 // FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
-	panic("Please implement the FavoriteCards function")
+	return []int{2, 6, 9}
 }
 
 // GetItem retrieves an item from a slice at given position.
