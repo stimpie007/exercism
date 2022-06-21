f1076d019c7f722e27eb7df9a94b0156a1382813
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-21 08:42:09 +0700

    Refactored duplicate code into new function OutOfBounds

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -1,5 +1,12 @@
 package cards
 
+func OutOfBounds(slice []int, index int) bool {
+	if index < 0 || index >= len(slice) {
+		return true
+	}
+	return false
+}
+
 // FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
 	return []int{2, 6, 9}
@@ -8,7 +15,7 @@ func FavoriteCards() []int {
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
-	if index >= len(slice) || index < 0 {
+	if OutOfBounds(slice, index) {
 		return -1
 	}
 	return slice[index]
@@ -17,7 +24,7 @@ func GetItem(slice []int, index int) int {
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-	if index >= len(slice) || index < 0 {
+	if OutOfBounds(slice, index) {
 		return append(slice, value)
 	}
 	slice[index] = value
@@ -31,7 +38,7 @@ func PrependItems(slice []int, value ...int) []int {
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	if index >= len(slice) || index < 0 {
+	if OutOfBounds(slice, index) {
 		return slice
 	}
 	return append(slice[:index], slice[index+1:]...)
