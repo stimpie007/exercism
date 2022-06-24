7c739999b6eb635348d90e5810f62657321d8c4a
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-24 20:24:50 +0700

    Changed AddSecretIngredient code to replace the last string from myList by the last string from friendsList

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -19,7 +19,7 @@ func Quantities(layers []string) (int, float64) {
 }
 
 func AddSecretIngredient(friendsList, myList []string) {
-	myList = append(myList, friendsList...)
+	myList[len(myList)-1] = friendsList[len(friendsList)-1]
 }
 
 // TODO: define the 'ScaleRecipe()' function
