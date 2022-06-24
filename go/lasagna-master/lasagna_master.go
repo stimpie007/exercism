540e7c59cfa88a9948ae894516cce18ec54dfad5
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-24 19:46:11 +0700

    Added AddSecretIngredient code

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -2,7 +2,6 @@ package lasagna
 
 import "strings"
 
-// TODO: define the 'PreparationTime()' function
 func PreparationTime(layers []string, prepTime int) int {
 	if prepTime == 0 {
 		return 2
@@ -10,14 +9,15 @@ func PreparationTime(layers []string, prepTime int) int {
 	return len(layers) * prepTime
 }
 
-// TODO: define the 'Quantities()' function
 func Quantities(layers []string) (int, float64) {
 	layersString := strings.Join(layers, ",")
 	noodles := strings.Count(layersString, "noodles")
 	sauce := strings.Count(layersString, "sauce")
-	return noodles * 50, float64(sauce) * 0.02
+	return noodles * 50, float64(sauce) * 0.2
 }
 
-// TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendsList, myList []string) {
+	myList = append(myList, friendsList...)
+}
 
 // TODO: define the 'ScaleRecipe()' function
