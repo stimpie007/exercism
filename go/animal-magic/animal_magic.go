853b55aa2ae8709d4f09a8d1ff7737595c2b3af1
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-24 16:56:40 +0700

    Added GenerateWandEnergy code

--- go/animal-magic/animal_magic.go
+++ go/animal-magic/animal_magic.go
@@ -17,7 +17,7 @@ func RollADie() int {
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return rand.Float64()
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order
