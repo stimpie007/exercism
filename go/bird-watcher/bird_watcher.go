85a135b15de6d6e5485f24a54c91853261f26350
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-24 16:12:48 +0700

    Added TotalBirdCount code

--- go/bird-watcher/bird_watcher.go
+++ go/bird-watcher/bird_watcher.go
@@ -3,7 +3,11 @@ package birdwatcher
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-	panic("Please implement the TotalBirdCount() function")
+	total := 0
+	for _, v := range birdsPerDay {
+		total += v
+	}
+	return total
 }
 
 // BirdsInWeek returns the total bird count by summing
