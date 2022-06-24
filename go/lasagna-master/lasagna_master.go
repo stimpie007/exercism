794d71857b202af02e27ef76350049502abf488d
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-24 17:33:41 +0700

    Added Quantities code

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,8 +1,22 @@
 package lasagna
 
+import "strings"
+
 // TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, prepTime int) int {
+	if prepTime == 0 {
+		return 2
+	}
+	return len(layers) * prepTime
+}
 
 // TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	layersString := strings.Join(layers, ",")
+	noodles := strings.Count(layersString, "noodles")
+	sauce := strings.Count(layersString, "sauce")
+	return noodles * 50, float64(sauce) * 0.02
+}
 
 // TODO: define the 'AddSecretIngredient()' function
 
