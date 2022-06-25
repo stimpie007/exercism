ffa7f861e66ac7306f3282b80846a8659ed49b6c
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-25 15:08:59 +0700

    Added Units mapping

--- go/gross-store/gross_store.go
+++ go/gross-store/gross_store.go
@@ -2,7 +2,13 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	panic("Please implement the Units() function")
+	return map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728}
 }
 
 // NewBill creates a new bill.
