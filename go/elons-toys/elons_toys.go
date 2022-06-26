1fb5898eea28fa6f4e83b9d66bee836b2ad86339
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-26 17:24:55 +0700

    Added DisplayDistance code

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,5 +1,7 @@
 package elon
 
+import "fmt"
+
 // TODO: define the 'Drive()' method
 func (c *Car) Drive() {
 	c.battery -= c.batteryDrain
@@ -7,6 +9,9 @@ func (c *Car) Drive() {
 }
 
 // TODO: define the 'DisplayDistance() string' method
+func (c *Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
+}
 
 // TODO: define the 'DisplayBattery() string' method
 
