48ee595b6deacc8bf7704686806ccb3274c70210
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-27 16:10:58 +0700

    Removed the TODO comments

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -2,7 +2,6 @@ package elon
 
 import "fmt"
 
-// TODO: define the 'Drive()' method
 func (c *Car) Drive() {
 	if c.battery > c.batteryDrain {
 		c.battery -= c.batteryDrain
@@ -10,17 +9,14 @@ func (c *Car) Drive() {
 	}
 }
 
-// TODO: define the 'DisplayDistance() string' method
 func (c *Car) DisplayDistance() string {
 	return fmt.Sprintf("Driven %d meters", c.distance)
 }
 
-// TODO: define the 'DisplayBattery() string' method
 func (c *Car) DisplayBattery() string {
 	return fmt.Sprintf("Battery at %d%%", c.battery)
 }
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
 func (c *Car) CanFinish(trackDistance int) bool {
 	// Calculate the battery way
 	// distancePerKM := trackDistance / c.speed
