117155b98ed5366089b9c88760d164db7787c031
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-27 17:22:08 +0700

    Added Application code

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -2,7 +2,17 @@ package logs
 
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	app := map[rune]string{
+		'\u2757':     "recommendation",
+		'\U0001f50d': "search",
+		'\u2600':     "weather",
+	}
+	for _, char := range log {
+		if name, ok := app[char]; ok {
+			return name
+		}
+	}
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
