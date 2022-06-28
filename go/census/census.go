02316d1d6fe1fa732370b634baa4918b945620d6
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-28 12:11:04 +0700

    Added NewResident code

--- go/census/census.go
+++ go/census/census.go
@@ -10,7 +10,8 @@ type Resident struct {
 
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
-	panic("Please implement NewResident.")
+	return &Resident{Name: name, Age: age, Address: address}
+	// => &{Matthew Sanabria 29 map[street:Main St.]}
 }
 
 // HasRequiredInfo determines if a given resident has all of the required information.
