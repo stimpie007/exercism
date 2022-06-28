d5b0de3c27b4d946096a6615a48cf7cb77bfc8e7
stevenhiltrop <stevenhiltrop@gmail.com>
2022-06-28 14:57:35 +0700

    Changed NewResident return struct literal for readability

--- go/census/census.go
+++ go/census/census.go
@@ -10,7 +10,10 @@ type Resident struct {
 
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
-	return &Resident{Name: name, Age: age, Address: address}
+	return &Resident{
+		Name:    name,
+		Age:     age,
+		Address: address}
 }
 
 // HasRequiredInfo determines if a given resident has all of the required information.
