e32b5c3e78513d9d641cb6b9e0789867d83f8818
stevenhiltrop <stevenhiltrop@gmail.com>
2022-12-06 19:47:50 +1300

    One lined it to an anti-pythonic garbage solution

--- python/resistor-color-trio/resistor_color_trio.py
+++ python/resistor-color-trio/resistor_color_trio.py
@@ -1,16 +1,16 @@
 from typing import List
 
 COLORS = {
-    "black": 0,
-    "brown": 1,
-    "red": 2,
-    "orange": 3,
-    "yellow": 4,
-    "green": 5,
-    "blue": 6,
-    "violet": 7,
-    "grey": 8,
-    "white": 9
+    "black": "0",
+    "brown": "1",
+    "red": "2",
+    "orange": "3",
+    "yellow": "4",
+    "green": "5",
+    "blue": "6",
+    "violet": "7",
+    "grey": "8",
+    "white": "9"
 }
 
 
@@ -20,9 +20,5 @@ def label(colors: List[str]) -> str:
     :param colors: List of strings
     :return: str
     """
-    color_one, color_two = COLORS.get(colors[0]), COLORS.get(colors[1])
-    color_three = pow(10, COLORS.get(colors[2]))
-
-    color_code = int(str(color_one) + str(color_two)) * color_three
-
+    color_code = int(COLORS.get(colors[0]) + COLORS.get(colors[1])) * pow(10, int(COLORS.get(colors[2])))
     return f"{str(color_code)[:-3]} kiloohms" if color_code % 1e3 == 0 else f"{color_code} ohms"
