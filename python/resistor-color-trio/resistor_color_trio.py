403ac07941159c22ece4ff99b477e61820c5cf21
stevenhiltrop <stevenhiltrop@gmail.com>
2022-12-06 19:21:17 +1300

    Added resistor_color_trio solution

--- python/resistor-color-trio/resistor_color_trio.py
+++ python/resistor-color-trio/resistor_color_trio.py
@@ -1,2 +1,27 @@
+COLORS = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
+
 def label(colors):
-    pass
+    # (["blue", "grey", "brown"]), "680 ohms")
+
+    color_one = COLORS.get(colors[0])
+    color_two = COLORS.get(colors[1])
+    color_three = pow(10, COLORS.get(colors[2]))
+
+    color = int(str(color_one) + str(color_two)) * color_three
+
+    if str(color)[-3:] == '000':
+        return str(color)[:-3] + " kiloohms"
+    else:
+        return str(color) + " ohms"
