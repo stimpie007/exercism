3c31a11eebdc46f9d15015c4a369dd74d0a57f62
stevenhiltrop <stevenhiltrop@gmail.com>
2022-12-06 18:48:22 +1300

    Added add_missing_stops function

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -24,14 +24,14 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
 
 
 # TODO: define the 'add_missing_stops()' function
-def add_missing_stops():
+def add_missing_stops(route, **stops):
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
     :param: arbitrary number of stops.
     :return: dict - updated route dictionary.
     """
-    pass
+    return {**route, "stops": list(stops.values())}
 
 
 # TODO: define the 'extend_route_information()' function
