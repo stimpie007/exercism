25d432b3ddcf67392d21a4e473b303c22e840745
stevenhiltrop <stevenhiltrop@gmail.com>
2022-12-06 19:26:11 +1300

    Fixed PEP8 recommendations

--- python/resistor-color-trio/resistor_color_trio.py
+++ python/resistor-color-trio/resistor_color_trio.py
@@ -17,17 +17,15 @@ COLORS = {
 def label(colors: List[str]) -> str:
     """
     Return the number of color code
-
     :param colors: List of strings
     :return: str
     """
-    color_one = COLORS.get(colors[0])
-    color_two = COLORS.get(colors[1])
+    color_one, color_two = COLORS.get(colors[0]), COLORS.get(colors[1])
     color_three = pow(10, COLORS.get(colors[2]))
 
-    color = int(str(color_one) + str(color_two)) * color_three
+    color_code = int(str(color_one) + str(color_two)) * color_three
 
-    if str(color)[-3:] == '000':
-        return str(color)[:-3] + " kiloohms"
+    if str(color_code)[-3:] == '000':
+        return str(color_code)[:-3] + " kiloohms"
     else:
-        return str(color) + " ohms"
+        return str(color_code) + " ohms"
