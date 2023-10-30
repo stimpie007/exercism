351594a3960c3b1a0dc9a0e85597ee00ffddb730
stevenhiltrop <stevenhiltrop@gmail.com>
2023-10-30 13:46:09 +1300

    Added killer sudoku helper solution

--- python/killer-sudoku-helper/killer_sudoku_helper.py
+++ python/killer-sudoku-helper/killer_sudoku_helper.py
@@ -1,2 +1,6 @@
+import itertools
+
+
 def combinations(target, size, exclude):
-    pass
+    candidates = [i for i in range(1, min(target, 9) + 1) if i not in exclude]
+    return [list(combo) for combo in itertools.combinations(candidates, size) if sum(combo) == target]
