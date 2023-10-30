f90b708271d71bd932c213de8b44107c08f0788f
stevenhiltrop <stevenhiltrop@gmail.com>
2023-10-30 13:42:38 +1300

    Added proverb solution

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -1,2 +1,4 @@
-def proverb():
-    pass
+def proverb(*objs, qualifier=""):
+    lines = [f"For want of a {w} the {l} was lost." for w, l in zip(objs, objs[1:])]
+    qual = "" if not (objs and qualifier) else f"{qualifier} "
+    return [] if not objs else lines + [f"And all for the want of a {qual}{objs[0]}."]
